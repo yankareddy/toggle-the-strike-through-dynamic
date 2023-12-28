1f5dfe93167fe55bcc23ef99f9c2503f7273fb51
yankareddy <reddy.a111friend@gmail.com>
2023-12-29 04:31:22 +0530

    toggle-the-strike-through-dynamic

---
+++ Toggle the Strike Through.js
@@ -0,0 +1,21 @@
+let checkBoxWithLabelContainerEl = document.getElementById("checkBoxWithLabelContainer");
+let checkboxId = "checkbox";
+let labelId = "checkboxLabel";
+
+function onCheckboxStatusChange() {
+    checkboxLabelEl.classList.toggle("strike-through");
+}
+let checkboxInputEl = document.createElement("input");
+checkboxInputEl.type = "checkbox";
+checkboxInputEl.id = checkboxId;
+checkboxInputEl.onclick = function() {
+    onCheckboxStatusChange();
+}
+checkBoxWithLabelContainerEl.appendChild(checkboxInputEl);
+
+let checkboxLabelEl = document.createElement("label");
+checkboxLabelEl.classList.add("checkbox-label");
+checkboxLabelEl.setAttribute("for", checkboxId);
+checkboxLabelEl.id = labelId;
+checkboxLabelEl.textContent = "I am a label";
+checkBoxWithLabelContainerEl.appendChild(checkboxLabelEl);
